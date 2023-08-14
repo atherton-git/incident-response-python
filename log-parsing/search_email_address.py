197fa1a8d5a2dfd87f94d815e9601c4f8ec7a99e
atherton-git <119111530+atherton-git@users.noreply.github.com>
2023-08-14 13:38:46 +0100

    Update search_email_address.py

--- log-parsing/search_email_address.py
+++ log-parsing/search_email_address.py
@@ -2,7 +2,7 @@ import re
 
 def is_valid_email(address):
     # Regular expression pattern to match email addresses
-    email_pattern = r'\b[A-Za-z0-9._%+-]{1,64}@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+    email_pattern = r'\b[A-Za-z0-9._%+-]{1,64}@[A-Za-z0-9.-]{1,64}\.[A-Za-z]{2,7}\b'
     return re.search(email_pattern, address)
 
 def find_email_addresses_in_file(file_path):
