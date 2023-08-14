e2fda3da6c46b701d2c56144e2b281be62d7931f
atherton-git <119111530+atherton-git@users.noreply.github.com>
2023-08-14 15:34:12 +0100

    Update search_domain_name.py

--- log-parsing/search_domain_name.py
+++ log-parsing/search_domain_name.py
@@ -1,22 +1,18 @@
 import re
 
-def is_valid_domain(domain):
-    # Regular expression pattern to match domains (including subdomains)
-    domain_pattern = r'^(https?://)?([a-zA-Z0-9.-]+(:\d+)?)$'
-    return re.match(domain_pattern, domain)
+def contains_http_url(line):
+    # Regular expression pattern to match http:// or https://
+    url_pattern = r"(http://|https://)\S+"
+    return re.search(url_pattern, line)
 
-def find_domains_in_file(file_path):
+def find_http_urls_in_file(file_path):
     try:
         with open(file_path, 'r') as file:
             for line_number, line in enumerate(file, start=1):
-                domain_match = is_valid_domain(line.strip())
-                if domain_match:
-                    protocol = domain_match.group(1) or ''
-                    domain_with_port = domain_match.group(2)
-                    print(f"Line {line_number}: {protocol}{domain_with_port}")
-
+                if contains_http_url(line):
+                    print(f"Line {line_number}: {line.strip()}")  # Print line number and the entire line
     except FileNotFoundError:
         print(f"File not found: {file_path}")
 
 # Define the path of the file you wish to parse
-find_domains_in_file(input('Please enter the filename to search: '))
+find_http_urls_in_file(input('Please enter the filename to search: '))
