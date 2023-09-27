3634b8d89a391b23204d2c38ddf1169f3ad76a71
atherton-git <119111530+atherton-git@users.noreply.github.com>
2023-09-27 04:56:52 +0100

    Update parse_linux_timestamps.py

--- log-parsing/parse_linux_timestamps.py
+++ log-parsing/parse_linux_timestamps.py
@@ -11,10 +11,10 @@ def process_log_entry(timestamp_string, line, line_number, current_year, relativ
     # Check if the log payload has been processed before
     if line.strip() not in processed_payloads:
         timestamp = None
-        formats_to_try = [('%Y %b %d %H:%M:%S', f"{current_year} {timestamp_string}"),  # Example: '2023 Aug 09 00:33:21'
-                          ('%Y %b %d %H:%M:%S.%f', f"{current_year} {timestamp_string}"),  # Example: '2023 Aug 09 00:33:21.915900'
-                          ('%Y-%m-%d %H:%M:%S', timestamp_string),  # Example: '2020-07-07 19:33:19'
-                          ('%d/%b/%Y:%H:%M:%S', timestamp_string)]  # Example: '09/Aug/2023:04:44:03'
+        formats_to_try = [('%Y %b %d %H:%M:%S', f"{current_year} {timestamp_string}"),  # Example: '1900 Jan 01 12:00:00'
+                          ('%Y %b %d %H:%M:%S.%f', f"{current_year} {timestamp_string}"),  # Example: '1900 Jan 01 12:00:00.000000'
+                          ('%Y-%m-%d %H:%M:%S', timestamp_string),  # Example: '1900-01-01 12:00:00'
+                          ('%d/%b/%Y:%H:%M:%S', timestamp_string)]  # Example: '01/Jan/1900:12:00:00'
         
         for format_to_try, formatted_timestamp_string in formats_to_try:
             try:
@@ -36,10 +36,10 @@ current_year = datetime.now().year
 timestamps = []
 processed_payloads = set()
 
-patterns = [r'(\w{3} \d{1,2} \d{2}:\d{2}:\d{2})',  # Example: 'Aug 09 00:33:21'
-            r'(\w{3} \d{1,2} \d{2}:\d{2}:\d{2}\.\d{6})',  # Example: 'Aug 09 00:33:21.915900'
-            r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})',  # Example: '2020-07-07 19:33:19'
-            r'(\d{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2})']  # Example: '09/Aug/2023:04:44:03'
+patterns = [r'(\w{3} \d{1,2} \d{2}:\d{2}:\d{2})',  # Example: 'Jan 01 12:00:00'
+            r'(\w{3} \d{1,2} \d{2}:\d{2}:\d{2}\.\d{6})',  # Example: 'Jan 01 12:00:00.000000'
+            r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})',  # Example: '1900-01-01 12:00:00'
+            r'(\d{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2})']  # Example: '01/Jan/1900:12:00:00'
 
 print(f"Searching for log files in {log_directory} and its subdirectories...")
 
